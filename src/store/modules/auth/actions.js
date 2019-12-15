f514099df1f2339e2c5db18e5cf1f288e8bae33f
Claudemir Gomes dos Santos <claudemirgms@gmail.com>
2019-12-15 19:19:43 -0300

    save perfile

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -7,7 +7,7 @@ export function signInRequest(email, password) {
 
 export function signInSuccess(token, user) {
   return {
-    type: '@auth/SIGN_IN_REQUEST',
+    type: '@auth/SIGN_IN_SUCCESS',
     payload: { token, user },
   };
 }
