120f9e11310ff0ee93f0b04fd4066ff9b0c7355d
Claudemir Gomes dos Santos <claudemirgms@gmail.com>
2019-12-15 12:44:01 -0300

    input validations

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -19,6 +19,7 @@ export const Content = styled.div`
     display: flex;
     flex-direction: column;
     margin-top: 30px;
+
     input{
       background: rgba(0,0,0,0.1);
       border: 0;
@@ -30,6 +31,13 @@ export const Content = styled.div`
       &::placeholder {
         color: rgba(255,255,255, 0.7);
       }
+    }
+
+    span {
+      color: #fb6f91;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
     }
 
     button {
