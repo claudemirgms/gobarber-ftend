9c8fb8463a9aa83e05024b9debcb5c70757db969
Claudemir Gomes dos Santos <claudemirgms@gmail.com>
2019-12-15 10:03:45 -0300

    private routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
-import Dashboard from '../pages/Dashdoard';
+import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact componet={SignIn} />
-      <Route path="/register" componet={SignUp} />
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
 
-      <Route path="/dashboard" exact componet={Dashboard} />
-      <Route path="/profile" exact componet={Profile} />
-    </Switch>
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
+</Switch>
   );
 }
